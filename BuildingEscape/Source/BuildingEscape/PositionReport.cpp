86a19daba916e3c4461ff8cd7dec5285f9c3af4f
DESKTOP-7LAAI1U\Morphbreed <morphbreed@chello.at>
2017-02-19 19:36:58 +0100

    added ObjectName to PositionReport, turned camera by 180°

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -10,8 +10,6 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -20,7 +18,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("position report reporting for duty on %s"), *ObjectName);
 	
 }
 
